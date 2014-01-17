63059883ef964950b7eb08ab386d6bdc30ea5958
AlvaroRomero <alvaro@dvloop.com>
2014-01-17 11:33:10 -0300

    Hola Mundo en ExtJS

---
+++ app.js
@@ -0,0 +1,14 @@
+Ext.application({
+    name: 'HelloExt',
+    launch: function() {
+        Ext.create('Ext.container.Viewport', {
+            layout: 'fit',
+            items: [
+                {
+                    title: 'Hello ExtJs',
+                    html : 'Hello WORLD! - DvLoop'
+                }
+            ]
+        });
+    }
+});
